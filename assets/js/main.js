ab82d95b3190795e4e4192b637eaf5117a2f8fc7
s4m5apu3l <asperatuz@yandex.ru>
2023-10-26 00:46:05 +0600

    feat: done 1/3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,52 @@
 console.dir("main.js init");
 
 document.addEventListener("DOMContentLoaded", function () {
-    var banners = document.querySelectorAll(".l-banner__js");
+    const banners = document.querySelectorAll(".l-banner__js");
+    const teeth = document.querySelector(".l-teeth__img");
 
     function showBanner(index) {
         banners[index].classList.add("active");
+        let nextIndex = (index + 1) % banners.length;
         console.dir(index);
-        
-        if (index == 0) {
+        console.dir(nextIndex);
+
+        setTimeout(() => {
+            banners[index].classList.add("out-active");
             setTimeout(() => {
-                banners[index].classList.add("out-active");
-            }, 3000);
-        }
-        setTimeout(function () {
-            banners[index].classList.remove("active");
-            banners[index].classList.remove("out-active");
-            var nextIndex = (index + 1) % banners.length;
-            showBanner(nextIndex);
-        }, 4000); // Ждем 3 секунды перед переключением на следующий баннер
+                banners[index].classList.remove("active");
+                banners[index].classList.remove("out-active");
+                banners[nextIndex].classList.add("active");
+                setTimeout(() => {
+                    teeth.classList.add("add-teeth");
+                }, 1000)
+                setTimeout(() => {
+                    banners[nextIndex].classList.add("out-active-teeth");
+                }, 2000);
+            }, 500);
+        }, 3000);
+        // setTimeout(() => {
+        //     nextIndex.classList.add("active");
+        // }, 4000);
+        // if (index == 0) {
+        // }
+        // if (index == 1) {
+        //     setTimeout(() => {
+        //         teeth.classList.add("add-teeth");
+        //     }, 1000);
+        //     setTimeout(() => {});
+        // }
+        // setTimeout(() => {
+        //     teeth.classList.remove("add-teeth");
+        //     banners[index].classList.remove("active");
+        //     // banners[index].classList.remove("out-active");
+        //     // var nextIndex = (index + 1) % banners.length;
+        //     // showBanner(nextIndex);
+        // }, 5000);
     }
 
     // Начинаем с первого баннера
-    // showBanner(0);
+    showBanner(0);
+
 });
 
 // document.addEventListener("DOMContentLoaded", () => {
