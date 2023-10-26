25ce38cc1541207b34c9a764e2d6d4c6ebdcdfa5
s4m5apu3l <asperatuz@yandex.ru>
2023-10-26 10:39:30 +0600

    feat: refactoring code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,63 @@
-console.dir("main.js init");
 
-document.addEventListener("DOMContentLoaded", function () {
-    const banners = document.querySelectorAll(".l-banner__js");
-    const teeth = document.querySelector(".l-teeth__img");
-    const sale = document.querySelector(".l-sale");
+const banners = document.querySelectorAll(".l-banner__js");
+const teeth = document.querySelector(".l-teeth__img");
+const sale = document.querySelector(".l-sale");
 
-    function showBanner(index) {
-        banners[index].classList.add("active");
-        let nextIndex = (index + 1) % banners.length;
+function addClassWithTimeout(element, className, timeout) {
+    setTimeout(() => {
+        element.classList.add(className);
+    }, timeout);
+}
 
-        setTimeout(() => {
-            banners[index].classList.add("out-active");
-            setTimeout(() => {
-                banners[index].classList.remove("active");
-                banners[index].classList.remove("out-active");
-                banners[nextIndex].classList.add("active");
-                setTimeout(() => {
-                    teeth.classList.add("add-teeth");
-                }, 1000);
-                setTimeout(() => {
-                    banners[nextIndex].classList.add("out-active-teeth");
-                    setTimeout(() => {
-                        sale.classList.add("active-sale");
-                        setTimeout(() => {
-                            banners[nextIndex].classList.add("out-active");
-                            setTimeout(() => {
-                                banners[nextIndex].classList.remove(
-                                    "out-active"
-                                );
-                                banners[nextIndex].classList.remove("active");
-                                banners[nextIndex].classList.remove(
-                                    "out-active-teeth"
-                                );
-                                teeth.classList.remove("add-teeth");
-                                sale.classList.remove("active-sale");
-                                showBanner(0);
-                            }, 1000);
-                        }, 2000); 
-                    }, 100);
-                }, 2000);
-            }, 500);
-        }, 3000);
-    }
+function removeClassWithTimeout(element, className, timeout) {
+    setTimeout(() => {
+        element.classList.remove(className);
+    }, timeout);
+}
+
+function delay(timeout) {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+}
+
+async function animateBanner(index) {
+    banners[index].classList.add("active");
+    let nextIndex = (index + 1) % banners.length;
+
+    await delay(3000);
+
+    addClassWithTimeout(banners[index], "out-active", 0);
+    await delay(500);
+
+    banners[index].classList.remove("active", "out-active");
+    banners[nextIndex].classList.add("active");
+
+    await delay(1000);
+
+    teeth.classList.add("add-teeth");
+    await delay(1000);
+
+    banners[nextIndex].classList.add("out-active-teeth");
+    await delay(100);
+
+    sale.classList.add("active-sale");
+    await delay(2000);
+
+    banners[nextIndex].classList.add("out-active");
+    await delay(1000);
+
+    banners[nextIndex].classList.remove("out-active");
+    banners[nextIndex].classList.remove("active");
+    banners[nextIndex].classList.remove("out-active-teeth");
+
+    teeth.classList.remove("add-teeth");
+    sale.classList.remove("active-sale");
 
     showBanner(0);
-});
+}
+
+async function showBanner(index) {
+    while (true) {
+        await animateBanner(index);
+    }
+}
+showBanner(0);
