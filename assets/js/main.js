1e19a2ad0d7463dc7a7044ee9f8f27c92af9a22b
s4m5apu3l <asperatuz@yandex.ru>
2023-10-26 17:55:45 +0600

    feat: add second banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const bannerSection = document.querySelector(".l-banner-animate");
+    const bannerSectionSecond = document.querySelector(".l-banner-animate__second");
+    const banners = document.querySelectorAll(".l-banner__js");
+    const teeth = document.querySelector(".l-teeth__img");
+    const sale = document.querySelector(".l-sale");
 
-const banners = document.querySelectorAll(".l-banner__js");
-const teeth = document.querySelector(".l-teeth__img");
-const sale = document.querySelector(".l-sale");
+    if (bannerSection) {
+        function addClassWithTimeout(element, className, timeout) {
+            setTimeout(() => {
+                element.classList.add(className);
+            }, timeout);
+        }
 
-function addClassWithTimeout(element, className, timeout) {
-    setTimeout(() => {
-        element.classList.add(className);
-    }, timeout);
-}
+        function removeClassWithTimeout(element, className, timeout) {
+            setTimeout(() => {
+                element.classList.remove(className);
+            }, timeout);
+        }
 
-function removeClassWithTimeout(element, className, timeout) {
-    setTimeout(() => {
-        element.classList.remove(className);
-    }, timeout);
-}
+        function delay(timeout) {
+            return new Promise((resolve) => setTimeout(resolve, timeout));
+        }
 
-function delay(timeout) {
-    return new Promise((resolve) => setTimeout(resolve, timeout));
-}
+        async function animateBanner(index) {
+            banners[index].classList.add("active");
+            let nextIndex = (index + 1) % banners.length;
 
-async function animateBanner(index) {
-    banners[index].classList.add("active");
-    let nextIndex = (index + 1) % banners.length;
+            await delay(3000);
 
-    await delay(3000);
+            addClassWithTimeout(banners[index], "out-active", 0);
+            await delay(500);
 
-    addClassWithTimeout(banners[index], "out-active", 0);
-    await delay(500);
+            banners[index].classList.remove("active", "out-active");
+            banners[nextIndex].classList.add("active");
 
-    banners[index].classList.remove("active", "out-active");
-    banners[nextIndex].classList.add("active");
+            await delay(1000);
 
-    await delay(1000);
+            teeth.classList.add("add-teeth");
+            await delay(3000);
 
-    teeth.classList.add("add-teeth");
-    await delay(3000);
+            banners[nextIndex].classList.add("out-active-teeth");
+            await delay(100);
 
-    banners[nextIndex].classList.add("out-active-teeth");
-    await delay(100);
+            sale.classList.add("active-sale");
+            await delay(3000);
 
-    sale.classList.add("active-sale");
-    await delay(3000);
+            banners[nextIndex].classList.add("out-active");
+            await delay(1000);
 
-    banners[nextIndex].classList.add("out-active");
-    await delay(1000);
+            banners[nextIndex].classList.remove("out-active");
+            banners[nextIndex].classList.remove("active");
+            banners[nextIndex].classList.remove("out-active-teeth");
 
-    banners[nextIndex].classList.remove("out-active");
-    banners[nextIndex].classList.remove("active");
-    banners[nextIndex].classList.remove("out-active-teeth");
+            teeth.classList.remove("add-teeth");
+            sale.classList.remove("active-sale");
 
-    teeth.classList.remove("add-teeth");
-    sale.classList.remove("active-sale");
+            showBanner(0);
+        }
 
-    showBanner(0);
-}
-
-async function showBanner(index) {
-    while (true) {
-        await animateBanner(index);
+        async function showBanner(index) {
+            while (true) {
+                await animateBanner(index);
+            }
+        }
+        showBanner(0);
     }
-}
-showBanner(0);
+
+    if (bannerSectionSecond) {
+        console.dir("init");
+        
+    }
+});
