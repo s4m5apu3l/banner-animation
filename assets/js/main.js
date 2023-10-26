eb0bf76af64fbb151f8a4e4fc8816b7d31f25c9e
s4m5apu3l <asperatuz@yandex.ru>
2023-10-26 10:22:43 +0600

    feat: add all

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,11 @@ console.dir("main.js init");
 document.addEventListener("DOMContentLoaded", function () {
     const banners = document.querySelectorAll(".l-banner__js");
     const teeth = document.querySelector(".l-teeth__img");
+    const sale = document.querySelector(".l-sale");
 
     function showBanner(index) {
         banners[index].classList.add("active");
         let nextIndex = (index + 1) % banners.length;
-        console.dir(index);
-        console.dir(nextIndex);
 
         setTimeout(() => {
             banners[index].classList.add("out-active");
@@ -18,36 +17,85 @@ document.addEventListener("DOMContentLoaded", function () {
                 banners[nextIndex].classList.add("active");
                 setTimeout(() => {
                     teeth.classList.add("add-teeth");
-                }, 1000)
+                }, 1000);
                 setTimeout(() => {
                     banners[nextIndex].classList.add("out-active-teeth");
+                    setTimeout(() => {
+                        sale.classList.add("active-sale");
+                        setTimeout(() => {
+                            banners[nextIndex].classList.add("out-active");
+                            setTimeout(() => {
+                                banners[nextIndex].classList.remove(
+                                    "out-active"
+                                );
+                                banners[nextIndex].classList.remove("active");
+                                banners[nextIndex].classList.remove(
+                                    "out-active-teeth"
+                                );
+                                teeth.classList.remove("add-teeth");
+                                sale.classList.remove("active-sale");
+                                showBanner(0);
+                            }, 1000);
+                            // showBanner(nextIndex);
+                        }, 2000); 
+                    }, 100);
                 }, 2000);
             }, 500);
         }, 3000);
-        // setTimeout(() => {
-        //     nextIndex.classList.add("active");
-        // }, 4000);
-        // if (index == 0) {
-        // }
-        // if (index == 1) {
-        //     setTimeout(() => {
-        //         teeth.classList.add("add-teeth");
-        //     }, 1000);
-        //     setTimeout(() => {});
-        // }
-        // setTimeout(() => {
-        //     teeth.classList.remove("add-teeth");
-        //     banners[index].classList.remove("active");
-        //     // banners[index].classList.remove("out-active");
-        //     // var nextIndex = (index + 1) % banners.length;
-        //     // showBanner(nextIndex);
-        // }, 5000);
     }
 
-    // Начинаем с первого баннера
     showBanner(0);
-
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const banners = document.querySelectorAll(".l-banner__js");
+//     const teeth = document.querySelector(".l-teeth__img");
+//     const sale = document.querySelector(".l-sale");
+
+//     function showBanner(index) {
+//         banners[index].classList.add("active");
+//         let nextIndex = (index + 1) % banners.length;
+
+//         setTimeout(() => {
+//             banners[index].classList.add("out-active");
+//             setTimeout(() => {
+//                 banners[index].classList.remove("active");
+//                 banners[index].classList.remove("out-active");
+//                 banners[nextIndex].classList.add("active");
+//                 setTimeout(() => {
+//                     teeth.classList.add("add-teeth");
+//                 }, 1000);
+//                 setTimeout(() => {
+//                     banners[nextIndex].classList.add("out-active-teeth");
+//                     setTimeout(() => {
+//                         sale.classList.add("active-sale");
+//                     }, 100);
+//                 }, 2000);
+//             }, 500);
+//         }, 3000);
+//     }
+
+//     showBanner(0);
+// });
+
+// setTimeout(() => {
+//     nextIndex.classList.add("active");
+// }, 4000);
+// if (index == 0) {
+// }
+// if (index == 1) {
+//     setTimeout(() => {
+//         teeth.classList.add("add-teeth");
+//     }, 1000);
+//     setTimeout(() => {});
+// }
+// setTimeout(() => {
+//     teeth.classList.remove("add-teeth");
+//     banners[index].classList.remove("active");
+//     // banners[index].classList.remove("out-active");
+//     // var nextIndex = (index + 1) % banners.length;
+//     // showBanner(nextIndex);
+// }, 5000);
 
 // document.addEventListener("DOMContentLoaded", () => {
 //     const parentBanner = document.querySelector(".l-banner-animate");
